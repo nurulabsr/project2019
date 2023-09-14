8727e7b6099180d46e66887ac3a870306b288344
ruabsarnul <absar.ru@yandex.com>
2023-09-15 02:15:01 +0600

    updated

--- beggainner_practice/HelloWorldProgram/Helloworld/Inheritance/Shapes5/Shapes5.java
+++ beggainner_practice/HelloWorldProgram/Helloworld/Inheritance/Shapes5/Shapes5.java
@@ -8,28 +8,73 @@ class TwoDShape{
 
   }
 
-  TwoDShape(double a){
-    width =  height = a;
-  }
-
-  TwoDShape(double w, double h, String s){
+  TwoDShape(double w, double h){
     width = w;
     height = h;
     
   }
+
+  TwoDShape(double a){
+    width =  height = a;
+  }
+
+ double getWidth() { return width; }
+ double getHeight() { return height; }
+
+ void showDim() {
+    System.out.println("Width and height are " + width + " and " + height);
+   }
 
 }
 
 
 
 class Triangle extends TwoDShape{
+   private String style;
+  Triangle() {
+    super();
+    style = "none";
+   }
+    Triangle(String s, double w, double h){
+        super(w, h);
+        style = s;
+    }
 
+   Triangle(double x) {
+    super(x); // call superclass constructor
+    style = "filled";
+   }
+
+  double area() {
+    return getWidth() * getHeight() / 2;
+    }
+ void showStyle() {
+    System.out.println("Triangle is " + style);
+  }
 
 }
 
 
-public class Shapes5{
-    public static void main(String[] args){
-
-    }
+class Shapes5 {
+ public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        Triangle t2 = new Triangle("outlined", 8.0, 12.0);
+        Triangle t3 = new Triangle(4.0);
+        t1 = t2;
+        System.out.println("Info for t1: ");
+        t1.showStyle();
+        t1.showDim();
+        System.out.println("Area is " + t1.area());
+        System.out.println();
+        System.out.println("Info for t2: ");
+        t2.showStyle();
+        t2.showDim();
+        System.out.println("Area is " + t2.area());
+        System.out.println();
+        System.out.println("Info for t3: ");
+        t3.showStyle();
+        t3.showDim();
+        System.out.println("Area is " + t3.area());
+        System.out.println();
+ }
 }
