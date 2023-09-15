49bc92bc6a077e48d3fe8a6db6744c380a84b652
ruabsarnul <absar.ru@yandex.com>
2023-09-15 23:36:51 +0600

    updated

---
+++ beggainner_practice/HelloWorldProgram/Helloworld/Inheritance/Shapes7/Shapes7.java
@@ -0,0 +1,80 @@
+class TwoDShape {
+    private double width;
+    private double height;
+ // A default constructor.
+ TwoDShape() {
+    width = height = 0.0;
+ }
+
+ TwoDShape(double w, double h) {
+    width = w;
+    height = h;
+ }
+
+ TwoDShape(double x) {
+    width = height = x;
+ }
+ TwoDShape(TwoDShape ob) {
+    width = ob.width;
+    height = ob.height;
+ }
+ double getWidth() { return width; }
+ double getHeight() { return height; }
+//  void setWidth(double w) { width = w; }
+//  void setHeight(double h) { height = h; }
+ void showDim() {
+    System.out.println("Width and height are " + width + " and " + height);
+ }
+}
+// A subclass of TwoDShape for triangles.
+class Triangle extends TwoDShape {
+    private String style;
+ // A default constructor.
+ Triangle() {
+    super();
+    style = "none";
+ }
+
+ Triangle(String s, double w, double h) {
+    super(w, h); // call superclass constructor
+    style = s;
+ }
+
+ Triangle(double x) {
+    super(x); // call superclass constructor
+    style = "filled";
+ }
+ // Construct an object from an object.
+ Triangle(Triangle ob) {
+    super(ob); // pass object to TwoDShape constructor
+    style = ob.style; 
+ }
+ double area() {
+    return getWidth() * getHeight() / 2;
+ }
+ void showStyle() {
+    System.out.println("Triangle is " + style);
+ }
+}
+
+
+
+
+
+
+class Shapes7 {
+ public static void main(String[] args) {
+    Triangle t1 = new Triangle("outlined", 8.0, 12.0);
+    // make a copy of t1
+    Triangle t2 = new Triangle(t1);
+    System.out.println("Info for t1: ");
+    t1.showStyle();
+    t1.showDim();
+    System.out.println("Area is " + t1.area());
+    System.out.println();
+    System.out.println("Info for t2: ");
+    t2.showStyle();
+    t2.showDim();
+    System.out.println("Area is " + t2.area());
+    }
+}
